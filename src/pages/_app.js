66145689b6b9da84f9871659467ca0591cc50e90
Christina Lawrence <christina.lawrence@christinas-mbp.lan>
2024-07-05 17:36:43 -0700

    attempting to fix icon loading

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,9 @@
 import '@/styles/globals.css';
 
 import Layout from '../components/layout';
+import { config } from '@fortawesome/fontawesome-svg-core';
+
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
 	return (
