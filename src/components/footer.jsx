53787b20884e87edebd5ab8f6e12f79caf1c1433
Christina Lawrence <christina.lawrence@christinas-mbp.lan>
2024-07-05 21:25:27 -0700

    removing unnecessary links in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,24 +6,6 @@ const Footer = () => {
                 <p>
                     &copy; {new Date().getFullYear()} Christy's Portfolio
                 </p>
-                <div className="social_icons">
-                    <a
-                        href="https://github.com/olawanlejoel"
-                        aria-label="GitHub"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <i className="fa-brands fa-github"></i>
-                    </a>
-                    <a
-                        href="https://www.linkedin.com/in/olawanlejoel/"
-                        aria-label="LinkedIn"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <i className="fa-brands fa-linkedin-square"></i>
-                    </a>
-                </div>
             </div>
         </>
     )
