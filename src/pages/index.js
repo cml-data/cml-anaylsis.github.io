d642066094cc9296bf39bd12a54baeb9e9fa0172
Christina Lawrence <christina.lawrence@christinas-mbp.lan>
2024-07-05 21:23:15 -0700

    moving favicons

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ const Home = () => {
 				<title>Christina's Portfolio</title>
 				<meta name="description" content="Christina's Portfolio"/>
 				<meta name="viewport" content="width=device-width, initial-scale=1"/>
-				<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png"/>
-				<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png"/>
+				<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png"/>
+				<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png"/>
 			</Head>
 			<div>
 				<About/>
