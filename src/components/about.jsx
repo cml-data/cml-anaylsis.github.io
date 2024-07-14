2f4211fe159ea3189068c45e917a4c6a486f6618
Christina Lawrence <christina.lawrence@christinas-mbp.lan>
2024-07-14 12:05:39 -0700

    more about me text edits

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,20 +8,22 @@ const About = () => {
                 <div className="about-text">
                     <p>
                         My career path has taken twists and turns.
-                        I graduated with a degree in Computer Systems Engineering,
-                        but my first job was taking care of Sun Solaris systems for the
-                        CAD department at Microchip Technology, where I did everything from
-                        wiring up the network and replacing internal parts to writing automation scripts for various projects.
-                        I learned a lot about Unix, then Linux (as we moved on from Solaris) in this position and this
-                        knowledge has been invaluable as my career moved on.  It translates directly to understanding deployment pipelines
-                        and setting up EC2 servers in AWS.  I eventually moved on to a QA automation position with Blackboard and
-                        learned the good and bad about attempting to automate all of your browser testing.  I have a good understanding
-                        of the strengths and weaknesses of automation frameworks and a deep respect for writing testable code.  The last 10 years
-                        have been spent as a full stack developer at NWEA (now HMH), helping develop features for k-12 reading fluency testing platforms
-                        and writing bespoke, teacher and parent-based, data-driven visualizations for our underlying research and psychometric data.
-                        My strongest aspects as a developer are troubleshooting code-bases that are unfamiliar and designing solutions that are efficient and robust.
-                        In the past year, I have began moving my career toward data science and am currently completing a Masters in Data Science.  I love the data-driven
-                        aspect of my work and would like to dig more deeply into datasets to find answers that help people.
+                        I graduated with a degree in Computer Systems Engineering, but my first job was taking care of
+                        Sun Solaris systems for the CAD department at Microchip Technology, where I did everything from
+                        wiring up the network and replacing internal parts to writing automation scripts for various
+                        projects. I learned a lot about Unix, then Linux (as we moved on from Solaris) in this position
+                        and this knowledge has been invaluable as my career moved on. It translates directly to
+                        understanding deployment pipelines, Docker containers, and setting up EC2 servers in AWS. I eventually moved on to a
+                        QA automation position with Blackboard and learned the good and bad about attempting to automate
+                        all of your browser testing. I understand the strengths and weaknesses of automation frameworks
+                        and have a deep respect for writing testable code. The last 10 years have been spent as a full
+                        stack developer at NWEA (now HMH), helping develop features for k-12 reading fluency testing
+                        platforms and writing bespoke, teacher and parent-based, data-driven visualizations for our
+                        underlying research and psychometric data. My strongest aspects as a developer are
+                        troubleshooting unfamiliar code bases and designing efficient and robust solutions.
+                        In the past year, I have begun moving my career toward data science and am currently completing
+                        a master’s degree in Data Science. I love the data-driven aspect of my work and would like to
+                        dig more deeply into datasets to find answers that help people.
                     </p>
                 </div>
                 <div className="about-img">
