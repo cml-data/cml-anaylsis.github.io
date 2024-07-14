c21f8a10f5e2934e2221122bc929890ab5de89fa
Christina Lawrence <christina.lawrence@christinas-mbp.lan>
2024-07-14 11:48:35 -0700

    more about me text edits

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -14,11 +14,11 @@ const About = () => {
                         wiring up the network and replacing internal parts to writing automation scripts for various projects.
                         I learned a lot about Unix, then Linux (as we moved on from Solaris) in this position and this
                         knowledge has been invaluable as my career moved on.  It translates directly to understanding deployment pipelines
-                        and setting up EC2 servers in AWS.  From there, I moved on to a QA automation position with Blackboard and
+                        and setting up EC2 servers in AWS.  I eventually moved on to a QA automation position with Blackboard and
                         learned the good and bad about attempting to automate all of your browser testing.  I have a good understanding
                         of the strengths and weaknesses of automation frameworks and a deep respect for writing testable code.  The last 10 years
-                        been spent as a full stack developer at NWEA (now HMH), helping develop features for k-12 reading fluency testing platforms
-                        and writing bespoke, teacher and parent-based, data-driven visualizations for our underlying research data.
+                        have been spent as a full stack developer at NWEA (now HMH), helping develop features for k-12 reading fluency testing platforms
+                        and writing bespoke, teacher and parent-based, data-driven visualizations for our underlying research and psychometric data.
                         My strongest aspects as a developer are troubleshooting code-bases that are unfamiliar and designing solutions that are efficient and robust.
                         In the past year, I have began moving my career toward data science and am currently completing a Masters in Data Science.  I love the data-driven
                         aspect of my work and would like to dig more deeply into datasets to find answers that help people.
