39c24d88e6dbcd13c1f4eae964b024ef7ec2f666
Christina Lawrence <christina.lawrence@christinas-mbp.lan>
2024-07-14 17:09:46 -0700

    updating cards to have more info and links to other profiles in the program

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,26 +1,34 @@
 import { projectData } from './data.js';
 
+function projectItem(project) {
+  if (project.visible) {
+      return (
+          <div key={project.id} className="project-card">
+              <a href={project.gitHubLink} target="_blank">
+                  <div className="project-header">
+                      <img className="project-icon" src={project.img} alt="text"/>
+                      <div className="small-icons">
+                          <i className="fa-brands fa-github"></i>
+                      </div>
+                  </div>
+                  <h3 className="project-title">{project.title}</h3>
+              </a>
+              <p dangerouslySetInnerHTML={{__html: project.description}}></p>
+          </div>
+  )
+      ;
+  } else
+      return "";
+}
+
 const Projects = () => {
     return (
         <div className="projects-container">
             <h2>Projects</h2>
             <div className="projects-grid">
                 {projectData && projectData.map((project) => (
-                    <div className="project-card" key={project.id}>
-                        <div className="project-header">
-                            <a href={project.gitHubLink} target="_blank">
-                                <i className="fa-regular fa-folder-open folder-icon"></i>
-                            </a>
-                            <div className="small-icons">
-                                <a href={project.gitHubLink} target="_blank"><i className="fa-brands fa-github"></i></a>
-                            </div>
-                        </div>
-                        <a href={project.gitHubLink} target="_blank">
-                            <h3>{project.title}</h3>
-                            <p>{project.description}</p>
-                        </a>
-                    </div>
-                    ))
+                    projectItem(project)
+                ))
                 }
             </div>
         </div>
